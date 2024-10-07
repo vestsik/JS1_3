5fa88f206c70963067cf9f8511b6a82a0b3da4bb
Hana Veštšíková <vestsikovah@gmail.com>
2024-10-07 20:02:05 +0200

    verze 6

--- script.js
+++ script.js
@@ -24,4 +24,43 @@ const pocetDnuMesic = Number(prompt("Zadejte, kolik hodin v mesici pracujete", "
 document.body.innerHTML = "<p>Hruba mzda: " + hodinovka * pocetHodinDenne * pocetDnuMesic + "</p>"
 
 // OBJEKTY
-//ukol 3
+//ukol 1: Realitka
+
+const apartment = {
+    type: 'rent',
+    disposition: '3+1',
+    area: {
+      floorage: 100,
+      balcony: 2,
+      units: 'sqm',
+    },
+    city: 'Prague',
+    district: 'Old Town',
+    price: {
+      rent: 28000,
+      fees: {
+        water: 1000,
+        energy: 2500,
+        services: 560,
+      },
+      currency: 'czk',
+    },
+    ownership: 'personal',
+    condition: 'renovated',
+    status: 'free',
+    floor: 3,
+  };
+
+document.body.innerHTML = "<p>Dispozice bytu: " + apartment.disposition + "</p>"
+document.body.innerHTML += "<p>Cisty najem bez poplatku: " + apartment.price.rent + " Kc</p>"
+document.body.innerHTML += "<p>Vymera bytu: " + apartment.area.floorage + " " + apartment.area.units + "</p>"
+
+const mesto = apartment.city
+const mestskaCast = apartment.district
+
+document.body.innerHTML += "<p>Mesto: " + mesto + ", Mestska cast: " + mestskaCast + "</p>"
+
+apartment.status = "taken"
+
+document.body.innerHTML += "<p>Status bytu: " + apartment.status + "</p>"
+
