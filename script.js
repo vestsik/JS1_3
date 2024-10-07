a758431d59aa0be346451e33ef3695e5f06e2801
Hana Veštšíková <vestsikovah@gmail.com>
2024-10-07 18:55:42 +0200

    verze 2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+const jmeno = prompt("Uvedte Vase jmeno a prijmeni");
+const vek = Number(prompt("Uvedte Vas vek"));
+
+document.body.innerHTML = "<p>" + jmeno + ", vek: " + vek + "</p>"
+
+
+/*
+cyklus ukládání na gitHub:
+git add .
+git commit -m "text"
+git push
+*/
