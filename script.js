251782bd2c16b0eed191110cc6f0a0af4822ebb3
Hana Veštšíková <vestsikovah@gmail.com>
2024-10-07 19:08:22 +0200

    verze 3

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const jmeno = prompt("Uvedte Vase jmeno a prijmeni");
-const vek = Number(prompt("Uvedte Vas vek"));
+document.body.innerHTML += "<p><b>ukol 1:</p>"
 
-document.body.innerHTML = "<p>" + jmeno + ", vek: " + vek + "</p>"
+const jmeno = prompt("Uvedte Vase jmeno a prijmeni", "Květoslav Voňavý");
+const vek = Number(prompt("Uvedte Vas vek", "67"));
+
+document.body.innerHTML += "<p>" + jmeno + ", vek: " + vek + "</p>"
 
 
 /*
@@ -10,3 +12,13 @@ git add .
 git commit -m "text"
 git push
 */
+
+
+// ukol 2a
+document.body.innerHTML += "<p><b>ukol 2a:</p>"
+
+const hodinovka = Number(prompt("Zadejte hodinovou sazbu"))
+const pocetHodinDenne = Number(prompt("Zadejte pocet hodin, ktere denne pracujete"))
+const pocetDnuMesic = Number(prompt("ZAdejte, kolik hodin v mesici pracujete"))
+
+document.body.innerHTML = "<p>Hruba mzda: " + hodinovka * pocetHodinDenne * pocetDnuMesic + "</p>"
